e9e9d1f47cdb1c03d3cc1748f1ae34db146dfec6
Igor Brizack <igor_brizack@hotmail.com>
2022-03-19 16:51:23 -0300

    nova versao

---
+++ script.js
@@ -0,0 +1,17 @@
+themes = [{
+    themeName: darkMode,
+    color: white,
+    backgroundColor: black
+},{
+    themeName: ligthMode,
+    color: black,
+    backgroundColor: white
+}];
+
+
+
+
+function addButtonTheme(){
+    const body = document.getElementsByTagName('body');
+    
+};
