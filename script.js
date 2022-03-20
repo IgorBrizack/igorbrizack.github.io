b2666f01fea97af47f7e54c6f3cc2b659ea03828
Igor Brizack <igor_brizack@hotmail.com>
2022-03-20 19:54:50 -0300

    adicionando funcoes do light mode

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-themes = [{
-    themeName: darkMode,
-    color: white,
-    backgroundColor: black
+let themes = [{
+    themeName: 'darkMode',
+    color: 'white',
+    backgroundColor: 'black'
 },{
-    themeName: ligthMode,
-    color: black,
-    backgroundColor: white
+    themeName: 'ligthMode',
+    color: 'black',
+    backgroundColor: 'white'  
 }];
 
 
+const buttonLight = document.getElementById('lightButton');
+buttonLight.addEventListener('click', ligthModeFunction);
 
+function ligthModeFunction (){
+    if(localStorage.getItem('Theme') == ('Theme', themes[0].themeName)){
+        localStorage.setItem('Theme', themes[1].themeName)
+        console.log('1')
+    } else {
+        localStorage.setItem('Theme', themes[0].themeName)
+        console.log('2')
+    }
+}
 
-function addButtonTheme(){
-    const body = document.getElementsByTagName('body');
+function defaultTheme(){
+    localStorage.setItem('Theme', themes[0].themeName);
     
-};
+    
+}
