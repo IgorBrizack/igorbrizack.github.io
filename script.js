81955189633ead5b8ce7bffb44d11289da031491
Igor Brizack <igor_brizack@hotmail.com>
2022-03-21 14:10:49 -0300

    cotinuando projeto

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let themes = [{
     themeName: 'darkMode',
-    color: 'white',
+    color: 'gray',
     backgroundColor: 'black'
 },{
     themeName: 'ligthMode',
@@ -15,15 +15,52 @@ buttonLight.addEventListener('click', ligthModeFunction);
 function ligthModeFunction (){
     if(localStorage.getItem('Theme') == ('Theme', themes[0].themeName)){
         localStorage.setItem('Theme', themes[1].themeName)
+        let colorTheme = themes[1].color
+        let colorBackground = themes[1].backgroundColor;
         console.log('1')
+        colorWords(colorTheme);
+        colorBackgroundFunction(colorBackground);
     } else {
-        localStorage.setItem('Theme', themes[0].themeName)
+        localStorage.setItem('Theme', themes[0].themeName);
+        let colorTheme = themes[0].color
+        let colorBackground = themes[0].backgroundColor;
+        colorWords(colorTheme);
+        colorBackgroundFunction(colorBackground);
         console.log('2')
     }
 }
 
-function defaultTheme(){
-    localStorage.setItem('Theme', themes[0].themeName);
-    
-    
+function colorWords(element){
+    let content = document.getElementsByClassName('content')[0];
+    console.log('content')
+    content.style.color = element;
 }
+
+function colorBackgroundFunction(element){
+    let content = document.getElementsByClassName('content')[0];
+    console.log('content')
+    content.style.backgroundColor = element;
+}
+
+function themeStorage(){
+    if(localStorage.getItem('Theme') == ('Theme', themes[0].themeName)){
+        localStorage.setItem('Theme', themes[0].themeName)
+        let colorTheme = themes[0].color
+        let colorBackground = themes[0].backgroundColor;
+        console.log('1')
+        colorWords(colorTheme);
+        colorBackgroundFunction(colorBackground);
+    } else {
+        localStorage.setItem('Theme', themes[1].themeName);
+        let colorTheme = themes[1].color
+        let colorBackground = themes[1].backgroundColor;
+        colorWords(colorTheme);
+        colorBackgroundFunction(colorBackground);
+        console.log('2')
+    }
+}
+
+onload = themeStorage();
+
+
+
